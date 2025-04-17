3fac35e1fb71a5799d3b467025e1ca761235dd1a
Ankush <psrankush@gmail.com>
2025-04-17 23:26:03 +0530

    Day 3 is Start approach 1

---
+++ Array/Day3.cpp
@@ -0,0 +1,22 @@
+// PROBLEM STATEMENT 3:
+// REVERSE AN ARRAY.
+
+// Given an array arr[], the task is to reverse the array. Reversing an array means rearranging the elements such that the first element becomes the last, the second element becomes second last and so on.
+
+// SAMPLE TEST CASE;
+
+/*INPUT : arr[] = {1, 4, 3, 2, 6, 5}
+OUTPUT: {5, 6, 2, 3, 4, 1}
+
+INPUT : arr[] = {4, 5, 1, 2}
+OUTPUT: {2, 1, 5, 4}*/
+
+// APPROACH 1: (NAVIE)
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[]={1, 4, 3, 2, 6, 5}
+}
